17e301d457e4b5b9b1f8c588f10ce6ebe33cfb86
FFad3 <gnebapatryk@gmail.com>
2023-10-04 22:26:58 +0200

    Add Extenstion for registration of FluentValidation, Automapper & MediatR

---
+++ CinemaSystem.Application/Extension.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CinemaSystem.Application
+{
+    public static class Extension
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssemblies();
+            });
+
+            return services;
+        }
+    }
+}
