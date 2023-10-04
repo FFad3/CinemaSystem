10086832cb2e877a494a7b3e4ce6efde30cfbb79
FFad3 <gnebapatryk@gmail.com>
2023-10-04 22:29:29 +0200

    Add MediatR  ResultLoggingBehaviour

---
+++ CinemaSystem.Infrastructure/RequestPipeline/ResultLoggingBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace CinemaSystem.Application.Abstraction.Requests
+{
+    internal class ResultLoggingBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IQuery<TResponse>, ICommand<TResponse>
+    {
+        private readonly ILogger<ResultLoggingBehaviour<TRequest, TResponse>> _logger;
+
+        public ResultLoggingBehaviour(ILogger<ResultLoggingBehaviour<TRequest, TResponse>> logger)
+        {
+            this._logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var result = await next();
+
+            var resultAsJson = JsonConvert.SerializeObject(result);
+
+            _logger.LogInformation("Request result: {resultAsJson}", resultAsJson);
+
+            return result;
+        }
+    }
+}
