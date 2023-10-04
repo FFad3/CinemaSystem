637d4d7b585a9978149557cbde9357156ed227d1
FFad3 <gnebapatryk@gmail.com>
2023-10-04 22:29:06 +0200

    Add MediatR  PereformenceBehaviour

---
+++ CinemaSystem.Infrastructure/RequestPipeline/PereformenceBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using CinemaSystem.Application.Abstraction.Infrastructure;
+using Humanizer;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace CinemaSystem.Application.Abstraction.Requests
+{
+    internal class PereformenceBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest
+    {
+        private readonly ILogger<PereformenceBehaviour<TRequest, TResponse>> _logger;
+        private readonly IClock _clock;
+
+        public PereformenceBehaviour(IClock clock, ILogger<PereformenceBehaviour<TRequest, TResponse>> logger)
+        {
+            this._clock = clock;
+            this._logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var requestName = typeof(TRequest).Name.Underscore();
+
+            var startTime = _clock.Current();
+            _logger.LogInformation("Begin request Name: {requestName}", requestName);
+
+            var result = await next();
+
+            var endTime = _clock.Current();
+
+            var elapsedMiliseconds = (startTime - endTime).TotalMilliseconds;
+
+            _logger.LogInformation("End request Name: {request} | Execution time: {time}", requestName, elapsedMiliseconds);
+
+            return result;
+        }
+    }
+}
