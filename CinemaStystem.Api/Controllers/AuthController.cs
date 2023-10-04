da4f0d0d8ba02de61efc87772ebcc5a01f847f4f
FFad3 <gnebapatryk@gmail.com>
2023-10-04 22:39:55 +0200

    Add Api project + auth contrroller WIP

---
+++ CinemaStystem.Api/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using CinemaSystem.Application.Features.Auth.Commands.SignUp;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaStystem.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(SignUp request)
+        {
+            await _mediator.Send(request);
+
+            return Ok();
+        }
+    }
+}
