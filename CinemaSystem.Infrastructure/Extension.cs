d50c34b28effab4e9dcfa839f24c225261c3fd3f
FFad3 <gnebapatryk@gmail.com>
2023-10-04 22:30:29 +0200

    Add extension for Infrastructure registration to DI

---
+++ CinemaSystem.Infrastructure/Extension.cs
@@ -0,0 +1,27 @@
+﻿using CinemaSystem.Application.Abstraction.Infrastructure;
+using CinemaSystem.Application.Abstraction.Requests;
+using CinemaSystem.Infrastructure.Security;
+using CinemaSystem.Infrastructure.Time;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CinemaSystem.Infrastructure
+{
+    internal static class Extension
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddSingleton<IClock, Clock>();
+
+            services.AddSeciurity();
+
+            services.ConfigureRequestPipeline(cfg =>
+            {
+                cfg.DisablePereformenceLogging = false;
+                cfg.DisableRequestPayloadLogging = false;
+                cfg.DisableRequestResultLogging = false;
+            });
+
+            return services;
+        }
+    }
+}
