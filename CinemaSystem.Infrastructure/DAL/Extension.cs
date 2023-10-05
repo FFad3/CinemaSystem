18219e54228ae9bc71ba42430fdf78c4c3427a61
FFad3 <gnebapatryk@gmail.com>
2023-10-05 19:16:58 +0200

    Add InMemoryUserRepository + registration

---
+++ CinemaSystem.Infrastructure/DAL/Extension.cs
@@ -0,0 +1,15 @@
+﻿using CinemaSystem.Core.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CinemaSystem.Infrastructure.DAL
+{
+    internal static class Extension
+    {
+        internal static IServiceCollection AddSql(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddScoped<IUserRepository, InMemoryUserRepository>();
+            return services;
+        }
+    }
+}
---
+++ CinemaSystem.Infrastructure/DAL/Repositories/InMemoryUserRepository.cs
@@ -0,0 +1,33 @@
+﻿using CinemaSystem.Core.Entities;
+using CinemaSystem.Core.Repositories;
+using CinemaSystem.Core.ValueObjects;
+
+namespace CinemaSystem.Infrastructure.DAL
+{
+    internal sealed class InMemoryUserRepository : IUserRepository
+    {
+        private readonly List<User> _users = new();
+
+        public InMemoryUserRepository()
+        {
+        }
+
+        public Task CreateAsync(User newUser, CancellationToken cancellationToken)
+        {
+            _users.Add(newUser);
+            return Task.CompletedTask;
+        }
+
+        public Task<User> GetByEmailAsync(Email email, CancellationToken cancellationToken)
+        {
+            var result = _users.SingleOrDefault(x => x.Email == email);
+            return Task.FromResult(result);
+        }
+
+        public Task<User> GetByUserNameAsync(UserName userName, CancellationToken cancellationToken)
+        {
+            var result = _users.SingleOrDefault(x => x.Username == userName);
+            return Task.FromResult(result);
+        }
+    }
+}
