ad85dcd82f228eff4d183d8fd7e7f636f480c06b
FFad3 <gnebapatryk@gmail.com>
2023-10-23 21:25:51 +0200

    Add Authhentication configuration

---
+++ CinemaSystem.Infrastructure/Security/AuthenticationConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace CinemaSystem.Infrastructure.Security
+{
+    internal sealed class AuthenticationConfiguration
+    {
+        public const string SectionName = "AuthenticationConfiguration";
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string AccessTokenSecret { get; set; }
+        public double AccessTokenExpirationInMinutes { get; set; }
+        public string RefreshTokenSecret { get; set; }
+        public double RefreshTokenExpirationInMinutes { get; set; }
+    }
+}
