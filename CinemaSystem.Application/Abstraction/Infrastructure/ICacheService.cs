ce02d429b68915d0d308f1f20ff3e9fcb88012d1
FFad3 <gnebapatryk@gmail.com>
2023-10-23 21:24:41 +0200

    Add Cache service

---
+++ CinemaSystem.Application/Abstraction/Infrastructure/ICacheService.cs
@@ -0,0 +1,11 @@
+﻿namespace CinemaSystem.Application.Abstraction.Infrastructure
+{
+    public interface ICacheService
+    {
+        Task<T?> GetAsync<T>(string key, CancellationToken cancellationToken = default) where T: class;
+
+        Task SetAsync<T>(string key, T value, CancellationToken cancellationToken = default) where T : class;
+
+        Task RemoveAsync<T>(string key, CancellationToken cancellationToken = default) where T : class;
+    }
+}
---
+++ CinemaSystem.Infrastructure/Cache/CacheService.cs
@@ -0,0 +1,43 @@
+﻿using CinemaSystem.Application.Abstraction.Infrastructure;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace CinemaSystem.Infrastructure.Cache
+{
+    public class CacheService : ICacheService
+    {
+        private readonly IDistributedCache _distributedCache;
+
+        public CacheService(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public async Task<T?> GetAsync<T>(string key, CancellationToken cancellationToken = default) where T : class
+        {
+            string? valueAsString = await _distributedCache.GetStringAsync(key, cancellationToken);
+
+            if (valueAsString is null)
+            {
+                return null;
+            }
+
+            T? value = JsonConvert.DeserializeObject<T>(valueAsString);
+
+            await _distributedCache.RefreshAsync(key, cancellationToken);
+
+            return value;
+        }
+
+        public async Task SetAsync<T>(string key, T value, CancellationToken cancellationToken = default) where T : class
+        {
+            string valueAsString = JsonConvert.SerializeObject(value);
+            await _distributedCache.SetStringAsync(key, valueAsString, cancellationToken);
+        }
+
+        public async Task RemoveAsync<T>(string key, CancellationToken cancellationToken = default) where T : class
+        {
+            await _distributedCache.RemoveAsync(key, cancellationToken);
+        }
+    }
+}
