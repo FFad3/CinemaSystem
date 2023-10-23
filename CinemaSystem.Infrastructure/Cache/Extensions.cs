730e6361204b9ebc06a2a2d3841c447a0aeb561b
FFad3 <gnebapatryk@gmail.com>
2023-10-23 21:26:26 +0200

    Add Cache registration extension

---
+++ CinemaSystem.Infrastructure/Cache/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using CinemaSystem.Application.Abstraction.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CinemaSystem.Infrastructure.Cache
+{
+    internal static class Extensions
+    {
+        internal static IServiceCollection AddCache(this IServiceCollection services)
+        {
+            services.AddDistributedMemoryCache();
+            services.AddSingleton<ICacheService, CacheService>();
+            return services;
+        }
+    }
+}
